ff58076e5904503dee7213d0d4e27ab6deb1ab3a
Mussab Kamhieh <Mussab Kamhieh@DESKTOP-6R3CUHO.clients.wireless.dtu.dk>
2018-01-04 14:04:04 +0100

    Alle gader fra felt 21 - 40. -Mussab

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -71,7 +71,67 @@ public class Board {
 		fields[22] = street15;
 		Street street16 = new Street(23, "Boardwalk", null, 5, Group.DARKBLUE);
 		fields[23] = street16;
-
+		
+		Parking parking = new Parking(21, "Parkering");
+		fields[21] = parking;
+		
+		
+		
+		
+		
+		Street street12 = new Street(22, "Trianglen", null, 5, Group.RED);
+		fields[22] = street12;
+		
+		Chance chance4 = new Chance(23, "Prøv lykken");
+		fields[23] = chance4;
+		
+		Street street13 = new Street(24, "Østerbrogade", null, 5, Group.RED);
+		fields[24] = street13;
+		Street street14 = new Street(25, "Grønningen", null, 5, Group.RED);
+		fields[25] = street14;
+		
+		Scandlines scandlines3 = new Scandlines(26, "Scandlines", "????");
+		fields[26] = scandlines3;
+		
+		Street street15 = new Street(27, "Bredgade", null, 6, Group.WHITE);
+		fields[27] = street15;
+		Street street16 = new Street(28, "Kgs. Nytorv", null, 6, Group.WHITE);
+		fields[28] = street16;
+		
+		Beverages Beverages2 = new Beverages(29, "Coca Cola");
+		fields[29] = Beverages2;
+		
+		Street street17 = new Street(30, "Østergade", null, 6, Group.WHITE);
+		fields[30] = street17;
+		
+		Jail jail2 = new Jail(31, "De fængsles", "Fængsel");
+		fields[31] = jail2;
+		
+		Street street18 = new Street(32, "Amagertorv", null, 7, Group.YELLOW);
+		fields[32] = street18;
+		Street street19 = new Street(33, "Vimmelskaffet", null, 7, Group.YELLOW);
+		fields[33] = street19;
+		
+		Chance chance5 = new Chance(34, "Prøv lykken");
+		fields[34] = chance5;
+		
+		Street street20 = new Street(35, "Nygade", null, 7, Group.YELLOW);
+		fields[35] = street20;
+		
+		Scandlines scandlines4 = new Scandlines(36, "Scandlines", "????");
+		fields[36] = scandlines4;
+		
+		Chance chance6 = new Chance(37, "Prøv lykken");
+		fields[37] = chance6;
+		
+		Street street21 = new Street(38, "Frederiksberggade", null, 8, Group.PURPLE);
+		fields[38] = street21;
+		
+		Beskatning beskatning1 = new Beskatning(39, "Ekstraordinær statsskat", "Betal Kr. 2.000");
+		fields[39] = beskatning1;
+		
+		Street street22 = new Street(40, "Rådhuspladsen", null, 8, Group.PURPLE);
+		fields[40] = street22;
 	}
 
 	public Field getField(int i) {
