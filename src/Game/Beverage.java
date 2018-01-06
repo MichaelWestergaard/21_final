92f5379d6e406b8fed5abc3670a037e37bfcdbca
Alparslan B. Ari <burhanari@192.168.0.11>
2018-01-06 20:09:15 +0100

    Ejeren af felterne til beverage og ferries er blevet lavet

--- src/Game/Beverage.java
+++ src/Game/Beverage.java
@@ -2,9 +2,40 @@ package Game;
 
 public class Beverage extends Buyable {
 
-	public Beverage(int fieldNo, String name, Player owner, Group group, int price) {
+	int rent;
+
+	public Beverage(int fieldNo, String name, Player owner, Group group, int price, int rent) {
 		super(fieldNo, name, owner, group, price);
+		this.rent = rent; 
 	}
+	public void landOnField(Player player,int diceSum, int rent) {
+		int amountToPay = diceSum*rent; //renten der skal betales
 
+		Player owner = super.getOwner();
+		if (owner == null) {// hvis der er ingen ejer, så køber man feltet
+			if (player.getPoints() >= super.getPrice()) {
+				player.addPoints(super.getPrice() * -1);
+				setOwner(player);
+			} 
+			else {
+				player.setBankrupt(true); //ændre til pantsat
+			}
+		} 
+		else { // Hvis der er en ejer, så betaler man
 
+			if (player.getPoints() >= amountToPay || player.getPoints() >= (amountToPay * 2)) {
+				player.addPoints(amountToPay * -1);
+				owner.addPoints(amountToPay);
+			} else {
+				player.setBankrupt(true); //ændre til pantsat
+				
+			}
+		}
+		
+		
+		
+	}
 }
+
+
+
--- src/Game/Ferry.java
+++ src/Game/Ferry.java
@@ -2,8 +2,41 @@ package Game;
 
 public class Ferry extends Buyable {
 
-	public Ferry(int fieldNo, String name, Player owner, Group group, int price) {
+	int rent;
+
+	public Ferry(int fieldNo, String name, Player owner, Group group, int price, int rent) {
 		super(fieldNo, name, owner, group, price);
+		this.rent = rent;
 	}
-	
+	public void landOnField(Player player,int diceSum, int rent) {
+		//int amountToPay = 500; //renten der skal betales
+		
+		int[] færgepris = {500,1000,1500,2000};
+		
+		Player owner = super.getOwner();
+		
+		
+		if (owner == null) {// hvis der er ingen ejer, så køber man feltet
+			if (player.getPoints() >= super.getPrice()) {
+				player.addPoints(super.getPrice() * -1);
+				setOwner(player);
+			} 
+			else {
+				player.setBankrupt(true); //ændre til pantsat
+			}
+		} 
+		else { // Hvis der er en ejer, så betaler man
+
+			if (player.getPoints() >= amountToPay || player.getPoints() >= (amountToPay * 2)) {
+				player.addPoints(amountToPay * -1);
+				owner.addPoints(amountToPay);
+			} else if (player.getPoints()) {
+				player.setBankrupt(true); //ændre til pantsat
+
+			}
+		}
+
+
+
+	}
 }
