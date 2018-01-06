5f1a80f4cc01bb5b0a7dffbdb4fd5dd44dfa4a45
Alen Hasanagic <Alen Hasanagic@LAPTOP-M0P1752P>
2018-01-06 19:41:14 +0100

    Street med huse/hotel priser

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -8,12 +8,34 @@ public class Board {
 
 	public void createFields() {
 
+		
 		Start start = new Start(0, 4000, "Start");
 		fields[0] = start;
 		
 		Street[] streets = new Street[] {
 			new Street(1, "Rødovrevej", null, Group.LIGHTBLUE, 1200, new int[] {50, 250, 750, 2250, 4000, 6000}, 1000),
-			new Street(3, "Hvidovrevej", null, Group.LIGHTBLUE, 1200, new int[] {50, 250, 750, 2250, 4000, 6000}, 1000)
+			new Street(3, "Hvidovrevej", null, Group.LIGHTBLUE, 1200, new int[] {50, 250, 750, 2250, 4000, 6000}, 1000),
+			new Street(6, "Roskildevej", null, Group.ORANGE, 2000, new int[] {100, 600, 1800, 5400, 8000, 11000}, 1000),
+			new Street(8, "Valby Langgade", null, Group.ORANGE, 2000, new int[] {100, 600, 1800, 5400, 8000, 11000}, 1000),
+			new Street(9, "Allégade", null, Group.ORANGE, 2400, new int[] {150, 800, 2000, 6000, 9000, 12000}, 1000),
+			new Street(11, "Frederiksberg Allé", null, Group.LIGHTGREEN, 2800, new int[] {200, 1000, 3000, 9000, 12500, 15000}, 2000),
+			new Street(11, "Frederiksberg Allé", null, Group.LIGHTGREEN, 2800, new int[] {200, 1000, 3000, 9000, 12500, 15000}, 2000),
+			new Street(13, "Bülowsvej", null, Group.LIGHTGREEN, 2800, new int[] {200, 1000, 3000, 9000, 12500, 15000}, 2000),
+			new Street(14, "Gl. Kongevej", null, Group.LIGHTGREEN, 3200, new int[] {250, 1250, 3750, 10000, 14000, 18000}, 2000),
+			new Street(16, "Bernstorffsvej", null, Group.LIGHTGREY, 3600, new int[] {300, 1400, 4000, 11000, 15000, 19000}, 2000),
+			new Street(18, "Hellerupvej", null, Group.LIGHTGREY, 3600, new int[] {300, 1400, 4000, 11000, 15000, 19000}, 2000),
+			new Street(19, "Strandvejen", null, Group.LIGHTGREY, 4000, new int[] {350, 1600, 4400, 12000, 16000, 20000}, 2000),
+			new Street(21, "Trianglen", null, Group.RED, 4400, new int[] {350, 1800, 5000, 14000, 17500, 21000}, 3000),
+			new Street(23, "Østerbrogade", null, Group.RED, 4400, new int[] {350, 1800, 5000, 14000, 17500, 21000}, 3000),
+			new Street(24, "Grønningen", null, Group.RED, 4800, new int[] {400, 2000, 6000, 15000, 18500, 22000}, 3000),
+			new Street(26, "Bredgade", null, Group.WHITE, 5200, new int[] {450, 2200, 6600, 16000, 19500, 23000}, 3000),
+			new Street(27, "Kgs. Nytorv", null, Group.WHITE, 5200, new int[] {450, 2200, 6600, 16000, 19500, 23000}, 3000),
+			new Street(29, "Østergade", null, Group.WHITE, 5600, new int[] {500, 2400, 7200, 17000, 20500, 24000}, 3000),
+			new Street(31, "Amagertorv", null, Group.YELLOW, 6000, new int[] {550, 2600, 7800, 18000, 22000, 25000}, 4000),
+			new Street(32, "Vimmelskaffet", null, Group.YELLOW, 6000, new int[] {550, 2600, 7800, 18000, 22000, 25000}, 4000),
+			new Street(34, "Nygade", null, Group.YELLOW, 6400, new int[] {600, 3000, 9000, 20000, 24000, 28000}, 4000),
+			new Street(37, "Frederiksberggade", null, Group.PURPLE, 7000, new int[] {800, 3500, 10000, 22000, 26000, 30000}, 4000),
+			new Street(39, "Rådhuspladsen", null, Group.PURPLE, 8000, new int[] {1000, 4000, 12000, 28000, 34000, 40000}, 4000),
 		};
 		
 		//Sætter alle ejendomme ind i fields[]
@@ -35,95 +57,39 @@ public class Board {
 			fields[chances[i].getFieldNo()] = chances[i];
 		}
 		
-		//AlenDK laver det her..
 		
 		Taxation taxation1 = new Taxation(4, "Betal inkomstskat", "10% eller Kr. 4.000");
 		fields[4] = taxation1;
 
-		Ferry ferries1 = new Ferry(5, "Scandlines", "Helsingør-helsinborg");
-		fields[5] = ferries1;
-		
-		Street street3 = new Street(6, "Roskildevej", null, 2, Group.ORANGE);
-		fields[6] = street3;
-		
-		
-		Street street4 = new Street(8, "Valby Langgade", null, 2, Group.ORANGE);
-		fields[8] = street4;
-		Street street5 = new Street(9, "Allégade", null, 2, Group.ORANGE);
-		fields[9] = street5;
-		
-		Jail jail1 = new Jail(10, "På besøg", "I fængsel");
-		fields[10] = jail1;
-		
-		Street street6 = new Street(11, "Frederiksberg Allé", null, 3, Group.LIGHTGREEN);
-		fields[11] = street6;
-		
-		Beverage beverages1 = new Beverage(12, "Squash");
-		fields[12] = beverages1;
-		
-		Street street7 = new Street(13, "Bülowsvej", null, 3, Group.LIGHTGREEN);
-		fields[13] = street7;
-		Street street8 = new Street(14, "Gl. Kongevej", null, 3, Group.LIGHTGREEN);
-		fields[14] = street8;
-
-		Ferry ferries2 = new Ferry(15, "Scandlines", "Mols linjen");
-		fields[15] = ferries2;
-		
-		Street street9 = new Street(16, "Bernstorffsvej", null, 4, Group.LIGHTGREY);
-		fields[16] = street9;
-		
-		Street street10 = new Street(18, "Hellerupvej", null, 4, Group.LIGHTGREY);
-		fields[18] = street10;
-		Street street11 = new Street(19, "Strandvejen", null, 4, Group.LIGHTGREY);
-		fields[19] = street11;	
-		
-		Parking parking = new Parking(20, "Parkering");
-		fields[20] = parking;
-		
-		Street street12 = new Street(21, "Trianglen", null, 5, Group.RED);
-		fields[21] = street12;
-		
-		Street street13 = new Street(23, "Østerbrogade", null, 5, Group.RED);
-		fields[23] = street13;
-		Street street14 = new Street(24, "Grønningen", null, 5, Group.RED);
-		fields[24] = street14;
-
-		Ferry ferries3 = new Ferry(25, "Scandlines", "Gedser-Rostock");
-		fields[25] = ferries3;
-		
-		Street street15 = new Street(26, "Bredgade", null, 6, Group.WHITE);
-		fields[26] = street15;
-		Street street16 = new Street(27, "Kgs. Nytorv", null, 6, Group.WHITE);
-		fields[27] = street16;
-		
-		Beverage beverages2 = new Beverage(28, "Coca Cola");
-		fields[28] = beverages2;
-		
-		Street street17 = new Street(29, "Østergade", null, 6, Group.WHITE);
-		fields[29] = street17;
-		
-		Jail jail2 = new Jail(30, "De fængsles", "Fængsel");
-		fields[30] = jail2;
-		
-		Street street18 = new Street(31, "Amagertorv", null, 7, Group.YELLOW);
-		fields[31] = street18;
-		Street street19 = new Street(32, "Vimmelskaffet", null, 7, Group.YELLOW);
-		fields[32] = street19;
-		
-		Street street20 = new Street(34, "Nygade", null, 7, Group.YELLOW);
-		fields[34] = street20;
-
-		Ferry ferries4 = new Ferry(35, "Scandlines", "Rødby Puttgarden");
-		fields[35] = ferries4;
-		
-		Street street21 = new Street(37, "Frederiksberggade", null, 8, Group.PURPLE);
-		fields[37] = street21;
-		
 		Taxation taxation2 = new Taxation(38, "Ekstraordinær statsskat", "Betal Kr. 2.000");
 		fields[38] = taxation2;
 		
-		Street street22 = new Street(39, "Rådhuspladsen", null, 8, Group.PURPLE);
-		fields[39] = street22;
+		Ferry ferries1 = new Ferry(5, "Scandlines", "Helsingør-helsinborg");
+		fields[5] = ferries1;
+		
+		Ferry ferries2 = new Ferry(15, "Scandlines", "Mols linjen");
+		fields[15] = ferries2;
+		
+		Ferry ferries3 = new Ferry(25, "Scandlines", "Gedser-Rostock");
+		fields[25] = ferries3;
+		
+		Ferry ferries4 = new Ferry(35, "Scandlines", "Rødby Puttgarden");
+		fields[35] = ferries4;
+		
+		Jail jail1 = new Jail(10, "På besøg", "I fængsel");
+		fields[10] = jail1;
+		Jail jail2 = new Jail(30, "De fængsles", "Fængsel");
+		fields[30] = jail2;	
+		
+		Beverage beverages1 = new Beverage(12, "Squash");
+		fields[12] = beverages1;
+		Beverage beverages2 = new Beverage(28, "Coca Cola");
+		fields[28] = beverages2;
+	
+		
+		Parking parking = new Parking(20, "Parkering");
+		fields[20] = parking;	
+		
 	}
 
 	public Field getField(int i) {
